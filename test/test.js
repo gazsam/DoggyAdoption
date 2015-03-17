028d9fd82f100f4034c3b42911e72da6bed69e4b
Sam Gazitt <gazitt@gmail.com>
2015-03-16 21:27:26 -0700

    tests back

--- test/test.js
+++ test/test.js
@@ -11,34 +11,34 @@ describe('node-env/sdk/environment', function (done) {
     done();
   }),
 
-  // it("Can list system environment variables", function () {
-  //   var env = sysInfo.sysInfo();
-  //   should.exist(env);
-  //   env.length.should.be.equal(5);
-  // });
+  it("Can list system environment variables", function () {
+    var env = sysInfo.sysInfo();
+    should.exist(env);
+    env.length.should.be.equal(5);
+  });
 
-  // it("Key processor leaves values unchanged", function () {
-  //   var result = keyProcessor.procKey('Name', 'VALUE');
-  //   result.should.be.equal('VALUE');
-  // });
+  it("Key processor leaves values unchanged", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE');
+    result.should.be.equal('VALUE');
+  });
 
-  // it("Key processor replaces \\r with an empty space", function () {
-  //   var result = keyProcessor.procKey('Name', 'VALUE\r');
-  //   result.should.be.equal('VALUE');
-  // });
+  it("Key processor replaces \\r with an empty space", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE\r');
+    result.should.be.equal('VALUE');
+  });
 
-  // it("Key processor replaces \\n with a <br/> tag", function () {
-  //   var result = keyProcessor.procKey('Name', 'VALUE\n');
-  //   result.should.be.equal('VALUE<br/>');
-  // });
+  it("Key processor replaces \\n with a <br/> tag", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE\n');
+    result.should.be.equal('VALUE<br/>');
+  });
 
-  // it("Key processor generates a <span> tag for the path variable", function () {
-  //   var result = keyProcessor.procKey('path', 'VALUE');
-  //   result.should.not.be.equal('VALUE');
-  // });
+  it("Key processor generates a <span> tag for the path variable", function () {
+    var result = keyProcessor.procKey('path', 'VALUE');
+    result.should.not.be.equal('VALUE');
+  });
 
-  // it("Key processor generates a <span> tag for the path variable", function () {
-  //   var result = keyProcessor.procKey('vmc_app_instance', '{"app_name" : "VALUE"}');
-  //   result.should.not.be.equal('VALUE');
-  // });
+  it("Key processor generates a <span> tag for the path variable", function () {
+    var result = keyProcessor.procKey('vmc_app_instance', '{"app_name" : "VALUE"}');
+    result.should.not.be.equal('VALUE');
+  });
 });
